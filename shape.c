ce4dd22020e3cfee0a0ab3bdd32f59d5bff1fd8f
DEWH <kjhg4321@naver.com>
2019-11-27 17:41:12 +0900

    shape api added
    main에 예시 코드 포함

---
+++ shape.c
@@ -0,0 +1,128 @@
+#include "shape.h"
+
+Cell get_Cell(Shape* shape, unsigned int x, unsigned int y) {
+	unsigned int index;
+	/* 보는 관점에서 가로 세로를 검사 */
+	if (x >= get_Width(shape) || y >= get_Height(shape)) {
+		fprintf(stderr, "get cell index out!\n");
+		/* 예외 처리 */
+		return nexist;
+	}
+	/* 보는 관점에 따라 인덱스 계산 */
+	switch (shape->direction) {
+	case north:
+		index = (shape->width - 1 - x) + shape->width * (shape->height - y);
+		break;
+	case east:
+		index = (shape->width - 1 - y) + x * shape->width;
+		break;
+	case south:
+		index = x + shape->width * y;
+		break;
+	case west:
+		index = y + shape->width * (shape->height - x);
+		break;
+	}
+	return shape->map[index];
+}
+unsigned int get_Height(Shape* shape) {
+	unsigned ret = (shape->direction == north || shape->direction == south) ? shape->height : shape->width;
+	return ret;
+}
+unsigned int get_Width(Shape* shape) {
+	unsigned ret = (shape->direction == north || shape->direction == south) ? shape->width : shape->height;
+	return ret;
+}
+void rotate_Clock(Shape* shape) {
+	shape->direction = (shape->direction + 1) % 4;
+}
+void rotate_CounterClock(Shape* shape) {
+	shape->direction = (shape->direction + 3) % 4;
+}
+Shape * make_Shape() {
+	Shape* shape;
+	Cell* map;
+	int tmp_index;
+	int tmp_x, tmp_y;
+	int width, height, size;
+	int i;
+	width = (rand() % 10) + 2;
+	height = (rand() % 10) + 2;
+	size = rand() % ((width * height)- 3) + 3;
+
+	shape = (Shape*)malloc(sizeof(Shape));
+	map = (Cell*)malloc(sizeof(Cell)*(width*height));
+
+	shape->width = width;
+	shape->height = height;
+	shape->map = map;
+	shape->direction = south;
+
+	/* 랜덤으로 모양 생성 */
+	tmp_index = rand() % (width * height);
+	map[tmp_index] = exist;
+
+	for (i = 1; i < size;) {
+		tmp_index = rand() % (width * height);
+		tmp_x = tmp_index % width;
+		tmp_y = tmp_index / width;
+		
+		if (map[tmp_index] == exist)
+			continue;
+
+		if (tmp_x >= 1 && map[tmp_index - 1] == exist) {
+			map[tmp_index] = exist;
+			i++;
+			continue;
+		}
+		else if (tmp_x < width - 1 && map[tmp_index + 1] == exist) {
+			map[tmp_index] = exist;
+			i++;
+			continue;
+		}
+		else if (tmp_y < height - 1 && map[tmp_index + width] == exist) {
+			map[tmp_index] = exist;
+			i++;
+			continue;
+		}
+		else if (tmp_y >= 1 && map[tmp_index - width] == exist) {
+			map[tmp_index] = exist;
+			i++;
+			continue;
+		}
+	}
+	return shape;
+}
+
+void delete_Shape(Shape* shape) {
+	free(shape->map);
+	free(shape);
+}
+/* for demo */
+void print_Shape(Shape* shape) {
+	int width = get_Width(shape);
+	int height = get_Height(shape);
+	int x, y;
+	for (x = 0; x < width; x++) {
+		for (int y = 0; y < height; y++) {
+			Cell tmp = get_Cell(shape, x, y);
+			if (tmp == exist)
+				printf("■");
+			else
+				printf("□");
+		}
+		printf("\n");
+	}
+}
+
+/* for demo */
+int main() {
+	srand(time(NULL));
+	for (int i = 0; i < 100; i++) {
+		Shape* test = make_Shape();
+		print_Shape(test);
+		delete_Shape(test);
+		getchar();
+	}
+	getchar();
+}
---
+++ shape.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/*
+ * 실제 모양을 회전시키는 것이 아니라,
+ * 보는 방향만 바꾼다.
+ *
+ *            NORTH
+ *              ↓
+ *          ㅁㅁㅁㅁㅁ
+ *  WEST → ㅁㅁㅁㅁㅁ ← EAST
+ *          ㅁㅁ  ㅁㅁ  
+ *              ↑
+ *            SOUTH(Default)
+ *
+ */
+/* For Direction definition */
+#define NORTH	0
+#define EAST	1
+#define SOUTH	2
+#define WEST	3
+/* Cekk definition */
+#define NEXIST	0
+#define EXIST	1
+
+typedef enum cell {
+	nexist = NEXIST,
+	exist = EXIST
+}Cell;
+
+typedef enum direction {
+	north = NORTH,
+	east = EAST,
+	south = SOUTH,
+	west = WEST
+}Direction;
+
+typedef struct shape {
+	unsigned int width;
+	unsigned int height;
+	Direction direction;
+	Cell* map;
+}Shape; // 모양과 크기를 정의함 ======== > 양원혁
+
+/* 방향에 따른 높이를 리턴 */
+unsigned int get_Height(Shape* shape);
+/* 방향에 따른 너비를 리턴 */
+unsigned int get_Width(Shape* shape);
+
+/*
+ * 방향을 기준으로 x,y 좌표의 셀을 리턴 
+ * example) 데모 코드 참조 
+ */
+Cell get_Cell(Shape* shape, unsigned int x, unsigned int y);
+/* 시계 방향으로 회전 */
+void rotate_Clock(Shape* shape);
+/* 반시계 방향으로 회전 */
+void rotate_CounterClock(Shape* shape);
+
+/* Shape 생성 */
+Shape * make_Shape(); //============ = > 양원혁
+/* Shape 제거 */
+void delete_Shape(Shape* shape);
+/* 데모용 프린트 */
+void print_Shape(Shape* shape);
