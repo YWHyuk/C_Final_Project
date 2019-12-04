42a560dabd03afe4be391bb704355f05ac0b2b01
TLHY <54972550+TLHY@users.noreply.github.com>
2019-12-04 19:21:50 +0900

    Update rent_kick_.c

--- rent_kick_.c
+++ rent_kick_.c
@@ -4,7 +4,7 @@ void scan_store(Player* player, Store* store,int id) {
 	
 	printf("%s      %d      %d      %d      %d\n", (store+(id-1))->name, (store + (id - 1))->money, (store + (id - 1))->rent, (store + (id - 1))->income, (store + (id - 1))->id);
 
-}
+}//가게 정보를 불러온다.
 
 void Floor_state(Player* player, Store* store,int x) {
 	int i = 0, j = 0;
@@ -26,7 +26,7 @@ void Floor_state(Player* player, Store* store,int x) {
 	}
 
 }
-
+//현재 임대된 공간을 보여준다(상점 구별x)
 void rent(Player* player, Store* store,int x) {
 	int size = (*(player->building) + x)->floor->width;
 	int sizes = size * size;
@@ -49,6 +49,8 @@ void rent(Player* player, Store* store,int x) {
 		print_Shape(store->shape, a, b);
 		printf("1.다시     2.결정\n");
 		scanf("%d", &menu);
+		printf("1.위치다시   2.시계회전   3.반시계회전  4.결정\n");
+		scanf("%d", &menu);
 		if (menu == 1) {
 			for (i = 0; i < size; i++) {
 				for (j = 0; j < size; j++) {
@@ -59,17 +61,29 @@ void rent(Player* player, Store* store,int x) {
 			}
 			continue;
 		}
-		
 		else if (menu == 2) {
-			if ((*(player->building) + x)->floor->cell[i + j]->rentrequest == 1) {
-				(*(player->building) + x)->floor->cell[i + j]->id = store->id;
-				break;
+
+			rotate_Clock(store->shape);
+
+		}
+		else if (menu == 3) {
+			rotate_CounterClock(store->shape);
+		}
+			else if (menu == 4) {
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if ((*(player->building) + x)->floor->cell[i + j]->rentrequest == 1) {
+						(*(player->building) + x)->floor->cell[i + j]->id = store->id;
+						(*(player->building) + x)->floor->cell[i + j]->rentrequest = 0;
+						break;
+					}
+				}
 			}
+
 		}
 	}
-
-
-}
+}//print_Shape에서 cell좌표에서 되도록 인자를 수정, 
+//rentrequest값이 1이 될 경우 그 부분에 id를 저장하고 이 동작이 완료되면 rentrequest는 전부 0으로 변경
 void kick(Player* player, Store* store, int x,int idnum) {
 	int size = (*(player->building) + x)->floor->width;
 	int i, j;
